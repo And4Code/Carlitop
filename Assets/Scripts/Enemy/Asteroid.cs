f73331cf83d8bc136a01fa87e87272b5b83fb01b
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2020-10-29 19:11:37 +0100

    Ajout ennemis

---
+++ Assets/Scripts/Enemy/Asteroid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public EnemyManager enemyManager;
+    public void Update()
+    {
+        Move();
+
+    }
+    public void Move()
+    {
+        Vector2 position = transform.position;
+
+        position = new Vector2(position.x, position.y - enemyManager.speed * Time.deltaTime);
+
+        transform.position = position;
+
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0.1f));
+
+        if (transform.position.y < min.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
