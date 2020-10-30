80c3fd8ae0de1d74a22750097e1c88cd832295e0
And4Code <Andrea.bois@e-artsup.net>
2020-10-30 12:11:38 +0100

    debut menus

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject PauseMenuUI;
+    private bool isPaused;
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Cursor.visible = !Cursor.visible;
+            Cursor.lockState = (Cursor.lockState == CursorLockMode.Locked) ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DesactivateMenu();
+
+        }
+    }
+
+    public void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        PauseMenuUI.SetActive(true);
+
+    }
+
+    public void DesactivateMenu()
+    {
+        Time.timeScale = 1;
+        PauseMenuUI.SetActive(false);
+        isPaused = false;
+
+    }
+    public void QuitGame()
+
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+}
